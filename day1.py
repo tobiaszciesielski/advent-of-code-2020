b8bd158077f65b8fa0587af0cf69acdc36d5381f
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2020-12-04 14:15:21 +0100

    Day1 refactor

--- day1.py
+++ day1.py
@@ -1,25 +1,30 @@
 
+def get_two_numbers():
+  for i in range(len(numbers)): 
+    for j in range(len(numbers)):
+      if numbers[i] + numbers[j] == 2020:
+        return numbers[i], numbers[j]
+
+def get_three_numbers():
+  for i in range(len(numbers)): 
+    for j in range(len(numbers)):
+      for k in range(len(numbers)):
+        if numbers[i] + numbers[j] + numbers[k] == 2020:
+          return numbers[i], numbers[j], numbers[k]
+
 if __name__ == "__main__":
   print("Advent of Code - day 1")
 
   with open("./input/day1.txt", mode="r") as file: 
-      numbers = [int(line) for line in file.read().splitlines()]
+    numbers = [int(line) for line in file.read().splitlines()]
 
   # part 1
 
-  for i in range(len(numbers)): 
-    for j in range(len(numbers)):
-      if i != j:
-        if numbers[i] + numbers[j] == 2020:
-          print(numbers[i] * numbers[j])
+  x, y = get_two_numbers()
+  print(x*y)
 
   # part 2
 
-  for i in range(len(numbers)): 
-    for j in range(len(numbers)):
-      if i != j:
-        for k in range(len(numbers)):
-          if j != k and i != k:
-            if numbers[i] + numbers[j] + numbers[k] == 2020:
-              print(numbers[i] * numbers[j] * numbers[k])
+  a, b, c = get_three_numbers()
+  print(a*b*c)
       
--- day2.py
+++ day2.py
@@ -5,26 +5,28 @@ print("Advent of Code - day 2")
 with open("./input/day2.txt", "r") as file:
   passwords = [line.split() for line in file.read().splitlines()]
 
-passwordCount = 0
-for i in range(len(passwords)):
-  line = passwords[i]
-  a, b = line[0].split('-')
-  letter = line[1][0]
-  password = line[2]
-  if int(a) <= password.count(letter) <= int(b):
-    passwordCount += 1
-
-print(passwordCount)
-
 # part 2
 
-passwordCount = 0
+password_count = 0
 for i in range(len(passwords)):
   line = passwords[i]
   a, b = line[0].split('-')
   letter = line[1][0]
   password = line[2]
   if (password[int(a)-1] == letter) ^ (password[int(b)-1] == letter):
-    passwordCount+=1
+    password_count+=1
     
-print(passwordCount)
+print(password_count)
+
+if __name__ == "__main__":
+
+  password_count = 0
+  for i in range(len(passwords)):
+    line = passwords[i]
+    a, b = line[0].split('-')
+    letter = line[1][0]
+    password = line[2]
+    if int(a) <= password.count(letter) <= int(b):
+      password_count += 1
+
+  print(password_count)
