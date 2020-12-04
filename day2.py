8864e34f7bf7decf02c532f8f42a22d67075ada7
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2020-12-04 14:25:50 +0100

    refactored day 2

--- day2.py
+++ day2.py
@@ -1,32 +1,28 @@
-'D:\Programy\python 3.9\python'
 
-print("Advent of Code - day 2")
-
-with open("./input/day2.txt", "r") as file:
-  passwords = [line.split() for line in file.read().splitlines()]
-
-# part 2
-
-password_count = 0
-for i in range(len(passwords)):
-  line = passwords[i]
-  a, b = line[0].split('-')
-  letter = line[1][0]
-  password = line[2]
-  if (password[int(a)-1] == letter) ^ (password[int(b)-1] == letter):
-    password_count+=1
-    
-print(password_count)
+def parse_password_template(pwd):
+  a, b = pwd[0].split('-')
+  letter = pwd[1][0]
+  password = pwd[2]
+  return (a, b, letter, password)
 
 if __name__ == "__main__":
+  print("Advent of Code - day 2")
 
-  password_count = 0
-  for i in range(len(passwords)):
-    line = passwords[i]
-    a, b = line[0].split('-')
-    letter = line[1][0]
-    password = line[2]
+  with open("./input/day2.txt", "r") as file:
+    passwords = [line.split() for line in file.read().splitlines()]
+
+  part_one_valid_passwords = 0
+  part_two_valid_password = 0
+  for password_template in passwords:
+    a, b, letter, password = parse_password_template(password_template)
+    
+    # part 1 
     if int(a) <= password.count(letter) <= int(b):
-      password_count += 1
+      part_one_valid_passwords += 1
+    
+    # part 2
+    if (password[int(a)-1] == letter) ^ (password[int(b)-1] == letter):
+      part_two_valid_password +=1
 
-  print(password_count)
+  print(part_one_valid_passwords)
+  print(part_two_valid_password)
