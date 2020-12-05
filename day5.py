5b5b106001780e8839c084eacf115580f08f0c51
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2020-12-05 14:15:48 +0100

    Day 5 part 1 solved

---
+++ day5.py
@@ -0,0 +1,40 @@
+
+from math import floor, ceil
+
+def read_input():
+  with open("./input/day5.txt") as file:
+    return file.read().splitlines()
+
+def count_position(seat_code, upper_value):
+  code = seat_code 
+  a, b = 0, upper_value-1
+  threshold = upper_value
+  code_lenght = len(seat_code)
+  for i in range(code_lenght):
+    half = code[i]
+    if half == 'F' or half == 'L': # lower half
+      if i == code_lenght-1: return a 
+      threshold = floor(threshold/2)
+      b -= threshold
+    elif half == 'B' or half == 'R': # upper half
+      if i == code_lenght-1: return b 
+      threshold = ceil(threshold/2)
+      a += threshold
+
+def count_position_id(seat_code):
+  total_rows, total_cols = 128, 8
+  row_code, col_code = seat_code[:7], seat_code[7:]
+  return count_position(row_code, total_rows) * total_cols + count_position(col_code, total_cols)
+
+if __name__ == "__main__":  
+  print("Advent of Code - day 5")
+
+  seat_codes = read_input()
+
+  # part 1
+
+  print(max([count_position_id(seat_code) for seat_code in seat_codes]))
+  
+  # part 2
+
+  # code ...
