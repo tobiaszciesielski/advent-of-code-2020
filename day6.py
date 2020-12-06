f75b4e81c9b1d54ee8930a37d60febdbb4ea5bb5
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2020-12-06 14:54:38 +0100

    Day 6 part 2 solved

--- day6.py
+++ day6.py
@@ -1,21 +1,38 @@
 
 def read_input():
   with open("./input/day6.txt") as file:
-    groups = file.read().split('\n\n')
-    for i in range(len(groups)):
-      groups[i] = groups[i].replace('\n', '')  
-    return groups
+    return [group_questions.splitlines() for group_questions in file.read().split('\n\n')]
 
+def collect_group_questions(group_questions):
+  return [set(''.join(string for string in answers)) for answers in group_questions]
+    
+def create_char_dictionary(characters):
+    char_dictionary = dict()
+    for character in characters:
+      char_dictionary[character] = 0
+    return char_dictionary
 
 if __name__ == "__main__":  
   print("Advent of Code - day _") # change day counter
 
-  groups = read_input()
+  group_questions = read_input()
 
   # part 1
-  
-  print(sum([len(set(group)) for group in groups]))
+
+  questions = collect_group_questions(group_questions)
+  print(sum([len(question) for question in questions]))
 
   # part 2
 
-  # code ...
+  counter = 0
+  for i in range(len(group_questions)):
+    char_dict = create_char_dictionary(questions[i])
+    current_questions = group_questions[i]
+    for person_questions in current_questions:
+      for question in person_questions:
+        char_dict[question]+=1
+    for question in char_dict.keys():
+     if len(current_questions) == char_dict[question]:
+       counter+=1
+
+  print(counter)
