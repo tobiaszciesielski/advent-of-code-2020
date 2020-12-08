709b621f9201b53bff2c91e4957d7c2040662949
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2020-12-08 13:15:58 +0100

    Day 8 part 2 solved

--- day8.py
+++ day8.py
@@ -1,4 +1,6 @@
 
+from copy import deepcopy
+
 def read_input():
   with open("./input/day8.txt") as file: 
     program = []
@@ -7,14 +9,14 @@ def read_input():
       program.append([op, int(val), False])
     return program
 
-
 if __name__ == "__main__":  
   print("Advent of Code - day8")
 
-  boot_code = read_input()
+  program = read_input()
 
   # part 1
   
+  boot_code = deepcopy(program)
   current_line = 0
   global_value = 0
   while current_line < len(boot_code):
@@ -32,4 +34,32 @@ if __name__ == "__main__":
   
   # part 2
 
-  # code ...
+
+  for i in range(len(program)):
+    boot_code = deepcopy(program) # 
+    
+    operation = boot_code[i][0]
+    if operation != 'acc':
+      boot_code[i][0] = 'nop' if operation == 'jmp' else 'jmp'
+    else: continue
+
+    current_line = 0
+    global_value = 0
+    terminates = True
+    while current_line < len(boot_code):
+      op, val, was_executed = boot_code[current_line]
+      if was_executed: 
+        terminates = False
+        break
+      boot_code[current_line][2] = True # was_executed to True
+      if op == 'jmp':
+        current_line+=val
+        continue
+      elif op == 'acc':
+        global_value+=val
+      current_line+=1
+    
+    if terminates:
+      break
+
+  print(global_value)
