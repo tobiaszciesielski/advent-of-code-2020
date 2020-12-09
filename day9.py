34983f00a9199de60cea444ca16d3c2f6f665124
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2020-12-09 13:25:42 +0100

    Day 9 part 2 solved

--- day9.py
+++ day9.py
@@ -11,13 +11,32 @@ if __name__ == "__main__":
   # part 1
   
   preamble = 25  
+  invalid_number = 0
   
   for i in range(preamble, len(numbers)):
-    array, number = numbers[i-preamble:i], numbers[i]
-    if not len([n for n in array if number - n in array]):
-      print(number) 
+    numbers_array, number = numbers[i-preamble:i], numbers[i]
+    if not len([n for n in numbers_array if number - n in numbers_array]):
+      invalid_number = number
       break
+
+  print(invalid_number)
 
   # part 2
 
-  # code ...
+  encryption_weakness = 0
+
+  for i in range(len(numbers)):
+    j = i+2 # at least two numbers
+    sum_of_numbers = 0
+    while sum_of_numbers < invalid_number:
+      if j+1 >= len(numbers): break
+      j+=1
+      numbers_array = numbers[i:j]
+      sum_of_numbers = sum(numbers_array)
+      if sum_of_numbers == invalid_number:
+        encryption_weakness = min(numbers_array)+max(numbers_array)
+        break
+    if encryption_weakness:
+      break
+
+  print(encryption_weakness)
